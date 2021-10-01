6c7d4ee9e64e43fcb5e629642c35750108df583c
ruben <rubenalia99@gmail.com>
2021-10-01 19:51:18 +0200

    comit medio avance de la nave
    no esta terminado el mov con restricciones ,falta conseguirlo y seguimiento camara

---
+++ “PR_ZAXXON_AliaRuben/Assets/scripts/NaveMov.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaveMov : MonoBehaviour
+{
+
+   public float Speed;
+    public float RtSpeed;
+    float limitH;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        Speed = 2f;
+        RtSpeed = 10f;
+        limitH =  1f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float desplazamientoX = Input.GetAxis("Horizontal");
+        float despY = Input.GetAxis("Vertical");
+        float despZ = Input.GetAxis("Profundidad");
+      
+   
+        transform.Translate(Vector3.right * Time.deltaTime * Speed *desplazamientoX,Space.World);
+        transform.Translate(Vector3.up * Time.deltaTime * RtSpeed * despY,Space.World);
+        transform.Translate(Vector3.back * Time.deltaTime * Speed * despZ);
+
+        float despR = Input.GetAxis("Rotation");
+        transform.Rotate(0f, 0f, despR * Time.deltaTime * RtSpeed);
+
+        float PosX = transform.position.x;
+        if((PosX < limitH || desplazamientoX < 0f)&&(PosX > -limitH || desplazamientoX  > 0f))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * Speed * desplazamientoX, Space.World);
+
+        }
+       
+    }
+}
