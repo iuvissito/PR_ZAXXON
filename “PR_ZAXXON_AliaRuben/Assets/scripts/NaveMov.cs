056d684c2bd1654cbcce8cacb9fd3d3263ca7dfc
ruben <rubenalia99@gmail.com>
2021-10-08 04:10:20 +0200

    actualizacion movr nave
    limites puestos comprobar profundidad con mando y poner limite rotacion

--- “PR_ZAXXON_AliaRuben/Assets/scripts/NaveMov.cs
+++ “PR_ZAXXON_AliaRuben/Assets/scripts/NaveMov.cs
@@ -8,6 +8,9 @@ public class NaveMov : MonoBehaviour
    public float Speed;
     public float RtSpeed;
     float limitH;
+    float limitV;
+    float limiteSuelo;
+    float limitZ;
     // Start is called before the first frame update
     
     void Start()
@@ -15,19 +18,27 @@ public class NaveMov : MonoBehaviour
         Speed = 2f;
         RtSpeed = 10f;
         limitH =  1f;
-        
-    }
+        limitV = 2f;
+        limiteSuelo = -1.5f;
+        limitZ = 3f;
+    
 
     // Update is called once per frame
     void Update()
-    {
+        {
+            MoverNave();
+        }
+
+        void MoverNave()
+        { 
+           
         float desplazamientoX = Input.GetAxis("Horizontal");
         float despY = Input.GetAxis("Vertical");
         float despZ = Input.GetAxis("Profundidad");
       
    
        // transform.Translate(Vector3.right * Time.deltaTime * Speed *desplazamientoX,Space.World);
-        transform.Translate(Vector3.up * Time.deltaTime * RtSpeed * despY,Space.World);
+       // transform.Translate(Vector3.up * Time.deltaTime * RtSpeed * despY,Space.World);
         transform.Translate(Vector3.back * Time.deltaTime * Speed * despZ);
 
         float despR = Input.GetAxis("Rotation");
@@ -39,6 +50,19 @@ public class NaveMov : MonoBehaviour
             transform.Translate(Vector3.right * Time.deltaTime * Speed * desplazamientoX, Space.World);
 
         }
-       
-    }
+            float posY = transform.position.y;
+            if ((posY < limitV || despY < 0f) && (posY > limiteSuelo || despY > 0f))
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * Speed * despY, Space.World);
+
+            }
+            float posZ = transform.position.z;
+            if ((posZ < limitZ || despZ < 0f) && (posZ > limiteSuelo || despZ > 0f))
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * RtSpeed * despZ);
+            }
+
+     }
 }
+}
+ 
